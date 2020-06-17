108785091aa2b828925559ac8387a33c0c4abcce
vaibhavi8 <vaibjhawar@gmail.com>
2020-06-17 15:35:39 -0700

    Updates

---
+++ precache-manifest.3a1437d9b2f46cdea3f19eb11cd6d190.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df22a7d88e9cb1b1c013606e7151799",
+    "url": "/mafia-online/index.html"
+  },
+  {
+    "revision": "023a616880459b20dd00",
+    "url": "/mafia-online/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "6b8926b43d2713f38148",
+    "url": "/mafia-online/static/js/2.024a3baa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mafia-online/static/js/2.024a3baa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "023a616880459b20dd00",
+    "url": "/mafia-online/static/js/main.cb781af4.chunk.js"
+  },
+  {
+    "revision": "5df206d3a57fca6b57ad",
+    "url": "/mafia-online/static/js/runtime-main.1937a3d5.js"
+  }
+]);
