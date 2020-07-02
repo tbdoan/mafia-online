614d178b840ad5dc408bb38bd331f9f65ce3ef0c
vaibhavi8 <vaibjhawar@gmail.com>
2020-07-02 12:26:09 -0700

    "Front end Styling"

--- src/main/frontend/src/components/StartButton.js
+++ src/main/frontend/src/components/StartButton.js
@@ -1,27 +1,22 @@
 import React, {useEffect} from 'react'
 
-const StartButton = ({stompClient, setPlayers, setGameState}) => {
+const StartButton = ({stompClient, setPlayers, start}) => {
 
     useEffect(() => {
         if(stompClient != null) {
-            const sub = stompClient.subscribe('/topic/startGame', (msg) => {
-                const players = JSON.parse(msg.body);
-                if(Object.keys(players).length === 0) {
-                    alert('Need 6 players to start');
-                } else {
-                    sub.unsubscribe();
-                    setPlayers(JSON.parse(msg.body));
-                    stompClient.send('/app/gameState', {}, 'night');
-                }
+            stompClient.connect({}, () => {});
+            const subscription = stompClient.subscribe('/topic/startGame', (msg) => {
+                setPlayers(JSON.parse(msg.body));
+                start(JSON.parse(msg.body));
+                subscription.unsubscribe();
             });
         }
     }, [stompClient])
 
     const onStart = (e) => {
         e.preventDefault();
-        stompClient.send('/app/startGame');
+        stompClient.send('/app/startGame', {}, '');
     }
-
     return (
         <div>
             <button onClick={onStart}>Start</button>
