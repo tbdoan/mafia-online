7b3aa0725711e3433e926c94f417d669465a332c
tbdoan <tbdoan@ucsd.edu>
2020-07-03 17:31:10 -0700

    moved frontend file up to root level

--- src/main/java/com/example/Mafia/MafiaApplication.java
+++ src/main/java/com/example/Mafia/MafiaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MafiaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MafiaApplication.class, args);
 	}
